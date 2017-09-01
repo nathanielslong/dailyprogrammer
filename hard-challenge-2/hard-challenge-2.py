31671b03aa3efba73ebfc7a3049edad7f1259ae1
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-08-31 20:41:35 -0400

    stopwatch made with object oriented approach

--- hard-challenge-2/hard-challenge-2.py
+++ hard-challenge-2/hard-challenge-2.py
@@ -2,35 +2,57 @@
 # while going, have ability to stop
 # lap ability
 # write to file
-import datetime
+from datetime import datetime
 
+class Stopwatch:
+    def __init__(self):
+        pass
 
-lap = 0
+    def start(self):
+        self.start = datetime.now()
+        print('Started at {}.'.format(self.start))
 
-def start():
-    start = datetime.datetime.now()
-    print('Stopwatch started at {}.'.format(start))
-    return start
+    def stop(self):
+        total = datetime.now() - self.start
+        print('Ended at {}. Total time: {}.'.format(datetime.now(), total))
 
-def stop(start):
-    end = datetime.datetime.now()
-    total = end - start
-    print('Time ended, total time: {}'.format(total))
+    def lap(self):
+        self.lap_start = datetime.now()
+        print('Lap started at {}.'.format(self.lap_start))
 
-def lap():
-    print('that')
+    def end_lap(self):
+        total = datetime.now() - self.lap_start
+        print('Lap ended at {}. Total time: {}.'.format(datetime.now(), total))
+
+def running_choices(stopwatch, lapped = False):
+    if not lapped:
+        choice = input('"s" to stop, "l" to lap\n')
+
+    else:
+        choice = input('"s" to stop completely, "l" to lap again,\
+                "e" to end current lap.\n')
+
+    if choice == 's':
+        stopwatch.stop()
+
+    elif choice == 'l':
+        stopwatch.lap()
+        running_choices(stopwatch, True)
+
+    elif choice == 'e' and lapped:
+        stopwatch.end_lap()
+        running_choices(stopwatch)
+
+    else:
+        running_choices(stopwatch)
 
 def main():
-        start_time = start()
-        options = {
-                'p' : stop,
-                'l' : lap
-                }
-        user_input = input('Stop with "p", lap with "l".\n')
-        while user_input not in options.keys():
-            print('Not recognized, try again.')
-            user_input = input('Stop with "p", lap with "l".\n')
+    start = input('Stopwatch: "s" to start.\n')
+    while start != 's':
+        start = input('Stopwatch: "s" to start.\n')
 
-        options[user_input](start_time)
+    stopwatch = Stopwatch()
+    stopwatch.start()
+    running_choices(stopwatch)
 
 if __name__ == '__main__' : main()
