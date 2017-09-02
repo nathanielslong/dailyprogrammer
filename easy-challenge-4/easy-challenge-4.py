53c26d2d7020a44fcb3f6a322bb752e9b2b51da4
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-09-01 20:58:33 -0400

    password generator finished

--- easy-challenge-4/easy-challenge-4.py
+++ easy-challenge-4/easy-challenge-4.py
@@ -1,16 +1,35 @@
 from random import randint
 
 def rand_char():
-    num = randint(33, 255)
+    num = randint(33, 127)
     return chr(num)
 
 def rand_pass(num):
     password = ''
     for i in range(0, int(num)):
         password += rand_char()
+    return password
+
+def write_pass(passwords):
+    with open('passwords.txt', 'w') as f:
+        for password in passwords:
+            f.write('{}\n'.format(password))
 
 def main():
-    random = rand_char()
-    print(random)
+    num_letters = input('Number of letters in password: ')
+    while not num_letters.isdigit():
+        num_letters = input('Not valid.\nNumber of letters in password: ')
+
+    num_pass = input('Number of passwords: ')
+    while not num_pass.isdigit():
+        num_pass = input('Not valid.\nNumber of passwords: ')
+
+    passwords = []
+    for x in range(0, int(num_pass)):
+        random = rand_pass(num_letters)
+        passwords.append(random)
+
+    write_pass(passwords)
+    print(passwords)
 
 if __name__ == '__main__' : main()
