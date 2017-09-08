518eb9ddcfb76c18898d6da401fd8cbb2c8840ba
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-09-07 20:21:24 -0400

    encryption works with no noted bugs, with large enough messages and ciphers could count as one time pad generator? could be user friendly for encrypt/decrypt option but this seems fine for right now

--- intermediate-challenge-3/intermediate-challenge-3.py
+++ intermediate-challenge-3/intermediate-challenge-3.py
@@ -7,7 +7,7 @@ class Crypt():
 
     def __make_cipher(self, number):
         for x in range(0, number):
-            self.__cipher.append(randint(32, 127))
+            self.__cipher.append(randint(32, 126))
 
     def encrypt(self, phrase, is_encrypt):
         count = 0
@@ -19,20 +19,30 @@ class Crypt():
                 count = 0
 
             if is_encrypt:
-                new_char = chr(ord(char) + self.__cipher[count])
+                new_char = chr((ord(char) + self.__cipher[count]) % 126)
             else:
-                new_char = chr(ord(char) - self.__cipher[count])
+                new_char = chr((ord(char) - self.__cipher[count]) % 126)
 
             new_phrase.append(new_char)
             count += 1
 
         return ''.join(new_phrase)
 
+def get_int(phrase):
+    val = input(phrase)
+    while not val.isdigit():
+        print('Not a positive integer, try again.')
+        val = input(phrase)
+    return abs(int(val))
+
 def main():
     phrase = input('Phrase to be encrypted: ')
-    crypt = Crypt(4)
+    cipher_size = get_int('Size of cipher: ')
+
+    crypt = Crypt(cipher_size)
     encrypted = crypt.encrypt(phrase, True)
     print(encrypted)
+
     decrypted = crypt.encrypt(encrypted, False)
     print(decrypted)
 
