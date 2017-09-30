fa9bf75332e92baa5458784c2ca41eb71a4bddef
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-09-30 12:26:38 -0400

    in the middle of shunting yard but getting frustrated, so moving to something else for a bit

--- intermediate-challenge-4/intermediate-challenge-4.py
+++ intermediate-challenge-4/intermediate-challenge-4.py
@@ -1,3 +1,29 @@
+tokens = {
+        '+' : [0, False]
+        '-' : [0, False],
+        '*' : 1,
+        '/' : 1,
+        '^' : 2,
+        '!' : 3,
+        '(' : 9,
+        ')' : 0
+        }
+
+def get_input(inp = None):
+    if inp is None:
+        inp = input('Expression: ')
+    value = inp.strip().split()
+    return value
+
+def shunting(expression):
+    number_queue = []
+    token_queue = []
+    for token in expression:
+        if token.isdigit():
+            number_queue.append()
+        elif token in tokens:
+
+
 def main():
     print('Hello world')
 
