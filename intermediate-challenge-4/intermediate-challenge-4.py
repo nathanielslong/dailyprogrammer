413204cd4c7d1592100f317d1d911dc2422aba30
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-11-03 21:42:03 -0400

    commiting changes just left around

--- intermediate-challenge-4/intermediate-challenge-4.py
+++ intermediate-challenge-4/intermediate-challenge-4.py
@@ -9,20 +9,25 @@ tokens = {
         ')' : [0, True]
         }
 
-def get_input(inp = None):
-    if inp is None:
-        inp = input('Expression: ')
-    value = inp.strip().split()
-    return value
+class Evaluator:
+    def __init__(self):
+        self.output = []
+        self.token = []
 
-def shunting(expression):
-    output_queue = []
-    token_queue = []
-    for token in expression:
-        if token.isdigit():
-            output_queue.append()
-        elif token in tokens:
-            if token[1] != true:
+    def get_input(inp = None):
+        if inp is None:
+            inp = input('Expression: ')
+        value = inp.strip().split()
+        return value
+
+    def shunting(expression):
+        output_queue = []
+        token_queue = []
+        for token in expression:
+            if token.isdigit():
+                output_queue.append()
+            elif token in tokens:
+                evaluate_token()
 
 def main():
     print('Hello world')
